25432b17688970b52c7eb3025a4519d73a6df97a
Alina <alina.weinberger@gmail.comgit config --global user.email alina.weinberger@gmail.com>
2025-03-03 14:41:49 +0100

    ordinal tranformer ok

--- MTA/ml_pipe.py
+++ MTA/ml_pipe.py
@@ -28,6 +28,7 @@ from sklearn.metrics import r2_score,  mean_absolute_error
 sys.path.append('/Users/alina/Desktop/MIT/code/ADHD/MTA/helper')
 from helper import  audit, ml
 from helper.transformers import CorrelationSelector,PreserveFeatureNames, PreserveFeatureNamesRegressor
+from helper.pipeline import make_pipeline
 
 mpl.rcParams['text.usetex'] = False
 
@@ -44,6 +45,8 @@ n_remove_top_features = None
 compute_perm_importances = False
 save_fig = False
 save_importance_df = False
+
+params = None # set if compute feature imprtance from existing result
 # Load the configuration from the file
 with open(args.config, 'r') as f:
     config = json.load(f)
@@ -63,11 +66,10 @@ for outcome_short, rater_out, rater_pred, model_type, corr_select, thr_drop_miss
     rater_pred = None if rater_pred == "None" else rater_pred
     
 
-
-
     outcome_dict = {'ODD':  "snap_snaoddt", "HYP": "snap_snahypat", "INATT" :"snap_snainatt" , "INTERN": "ssrs_sspintt", "SS": "ssrs_ssptosst", "DOM": "pcrc_pcrcpax", "INTIM": "pcrc_pcrcprx"}
 
     file_name_save = 'results_ML_simple_CV_RF_XGB_{}.csv'.format(outcome_short)
+    file_name_save_dist = 'dist_results_ML_simple_CV_RF_XGB_{}.csv'.format(outcome_short)
 
     if Path('/Volumes/Samsung_T5/MIT/mta').exists():
         data_root =     '/Volumes/Samsung_T5/MIT/mta'
@@ -79,28 +81,15 @@ for outcome_short, rater_out, rater_pred, model_type, corr_select, thr_drop_miss
     info_path = Path(data_root, "files") # dewcipion of vars as written out questions 
     save_path = Path(data_derived, 'ML_results')
     types_file_path = Path(data_derived,"all_vars_description_ML.xlsx" ) # deescription of variables aas ordinal, numeric or categorical 
-    file_path_save = Path(save_path, file_name_save) # path to save and load ML resluts tabke 
-
+    file_path_save_mean = Path(save_path, file_name_save) # path to save and load ML resluts tabke 
+    file_path_save_dist= Path(save_path, file_name_save_dist) 
 
     ################## DATA ####################
     pred = pd.read_csv(Path(data_derived, 'mta_data_clean.csv')).drop(columns = 'Unnamed: 0')
     out = pd.read_csv(Path(data_derived, 'out_clean_all_raters.csv')).drop(columns = 'Unnamed: 0')
 
 
-
-    # if use_params_from_result : # use parameters from the best model saved in ML results table ß
-    #     model_type,  corr_select,   thr_corr, params,  col_out, rater_pred, thr_drop_missing ,r2 = ml.get_params_from_best_result(file_path_save)
-    #     rater_out = col_out[-1]
-    # else: ################# get INPUT FROM USER 
-    #     model_type ="RandomForestRegressor" # "XGBRegressor"
-    #     corr_select = True
-    #     thr_corr = 0.8
-    #     params = None
-    #     rater_pred = None 
-    #     #rater_out = "m"
-    #     thr_drop_missing = 50
     col_out = outcome_dict[file_name_save.split(".")[0].split("_")[-1]] + "_"+ rater_out
-        
     outcome_var = outcome_dict[outcome_short]
 
 
@@ -124,70 +113,11 @@ for outcome_short, rater_out, rater_pred, model_type, corr_select, thr_drop_miss
 
     groups = data['src_subject_id'].values
 
-
-    if model_type == "XGBRegressor":
-        regress =  XGBRegressor(random_state=42)
-    elif model_type ==  "RandomForestRegressor":
-        regress = RandomForestRegressor(random_state= 42)
+    pipeline, param_distributions, = make_pipeline(model_type= model_type, corr_select= corr_select, 
+                                                        ord_vars= ord_vars,num_vars=num_vars,  cat_vars_str= cat_vars_str, 
+                                                        cat_vars_num = cat_vars_num, params= params)
 
 
-    num_pipe = Pipeline([
-        ('imputer', SimpleImputer(strategy='mean')), # check paper 
-        ('std_scaler',StandardScaler())
-    ])
-
-    ord_pipe = Pipeline([
-        ('imputer', PreserveFeatureNames(SimpleImputer(strategy='mean'))), # change that to OrdinalEncoder sklearn -- just encodes to catgoreies !!
-        ('identity', PreserveFeatureNames(FunctionTransformer(lambda x: x)))
-    ])
-
-
-    cat_str_pipe = Pipeline([
-        ('imputer', PreserveFeatureNames(SimpleImputer(strategy='constant', fill_value='missing'))),
-        ('ohe', PreserveFeatureNames(TableVectorizer()))
-    ])
-
-    cat_num_pipe = Pipeline([
-        ('imputer', PreserveFeatureNames(SimpleImputer(strategy='constant', fill_value=-1))),
-        ('ohe', PreserveFeatureNames(OneHotEncoder(handle_unknown="ignore")))
-    ])
-
-    preprocessor = ColumnTransformer([
-        ('num', num_pipe, num_vars),
-        ('cat_str', cat_str_pipe, cat_vars_str),
-        ('cat_num', cat_num_pipe, cat_vars_num),
-        ('ord', ord_pipe, ord_vars)
-    ])
-
-    if corr_select : 
-        pipeline = Pipeline(steps=[
-        ('preprocessor', preprocessor),
-        ('correlation_selector', CorrelationSelector(threshold=0.8)),
-        ('regressor',regress)
-        ])
-    else: 
-        pipeline = Pipeline(steps=[
-            ('preprocessor', preprocessor),
-            ('regressor',regress)
-        ])
-
-    if model_type == "RandomForestRegressor" :
-        param_distributions = {
-            'regressor__n_estimators': [100, 300],          # Correct prefix
-            'regressor__max_depth': [10, None],
-            'regressor__min_samples_split': [2, 10],
-            'regressor__min_samples_leaf': [1, 4]
-        }
-        
-    elif model_type == "XGBRegressor":
-        param_distributions = {
-            'regressor__n_estimators': [100, 300],
-            'regressor__max_depth': [10],                    # Removed None
-            'regressor__colsample_bytree': [0.5, 0.3],       # Mapped 'sqrt' and 'log2' to numeric values
-            'regressor__min_child_weight': [2, 10, 1, 4],    # Combined 'min_samples_split' and 'min_samples_leaf'
-        }
-        
-    # Define the parameter grid
 
     # Define the GroupKFold cross-validation
     cv = GroupKFold(n_splits=5)
@@ -205,7 +135,7 @@ for outcome_short, rater_out, rater_pred, model_type, corr_select, thr_drop_miss
         param_distributions=param_distributions,
         n_iter=10,  # Number of parameter settings to sample
         scoring=scorer,
-        refit='rmse', 
+        refit='r2', 
         cv=cv,
         verbose=3,
         random_state=42
@@ -219,8 +149,9 @@ for outcome_short, rater_out, rater_pred, model_type, corr_select, thr_drop_miss
     print("Best Score:", random_search.best_score_)
 
 
-    new_row = ml.get_results_from_random_search(random_search, outcome_short,rater_out,rater_pred,  thr_drop_missing)
-
+    new_row_mean = ml.get_results_from_random_search(random_search, outcome_short,rater_out,rater_pred,  thr_drop_missing)
+    new_row_dist = ml.get_results_from_random_search(random_search, outcome_short,rater_out,rater_pred,  thr_drop_missing, get_dist=True)
     # if verify_before save is set to True, save and reduced parameters will be ignored 
     # and asked again from user input after displaying the tables 
-    ml.save_cv_result_to_table(file_path_save, new_row, save=True, reduced= False, nrows2drop=None, verify_before_save=False)
+    ml.save_cv_result_to_table(file_path_save_mean, new_row_mean, save=True, reduced= False, nrows2drop=None, verify_before_save=False)
+    ml.save_cv_result_to_table(file_path_save_dist, new_row_dist, save=True, reduced= False, nrows2drop=None, verify_before_save=False)
